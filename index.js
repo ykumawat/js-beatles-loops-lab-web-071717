056bd7c16f421059d65c17da2522b745ca1efef7
Yamini Kumawat <yaminikumawat94@gmail.com>
2017-08-28 21:54:29 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function theBeatlesPlay(musicians, instruments) {
+  let newArray = []
+  for (let i=0; i < musicians.length; i++) {
+    newArray.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return newArray
+};
+
+function johnLennonFacts(facts) {
+  let newArray = []
+  let i = 0
+  while (i < facts.length) {
+    newArray.push(`${facts[i]}!!!`)
+    i++
+  }
+  return newArray
+};
+
+function iLoveTheBeatles(n) {
+  let newArray = []
+  do {
+    newArray.push("I love the Beatles!")
+    n++
+  } while (n < 15)
+  return newArray
+}
